46ea8dc8493d961917750645ad3a6802b97d942c
zl46547 <you@example.com>
2018-05-01 12:32:56 +0800

    查看购物车，收藏商品

--- src/components/productDetail/footer.vue
+++ src/components/productDetail/footer.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="footer">
-    <div class="icon">
+    <div class="icon" @click="toService">
       <i class="iconfont icon-kefu"></i>
       <p>客服</p>
     </div>
-    <div class="icon">
-      <i class="iconfont icon-favor"></i>
-      <p>收藏</p>
+    <div class="icon" @click="onLike">
+      <i class="iconfont icon-favor" :class="{myLikeProduct:isLikeProduct}"></i>
+      <p :class="{myLikeProduct:isLikeProduct}">收藏</p>
     </div>
-    <div class="icon">
+    <div class="icon" @click="toCartPage">
       <i class="iconfont icon-cart"></i>
-      <mt-badge size="small" type="error">10</mt-badge>
+      <mt-badge size="small" type="error">99+</mt-badge>
       <p>购物车</p>
     </div>
     <div class="addCart">
@@ -21,68 +21,110 @@
 
 <script type="text/ecmascript-6">
 
+  import Util from '../../util/common'
+
   export default {
     props: ['response'],
-    data() {
+    data () {
       return {
+        productId: '',
+        isLikeProduct: false
       }
     },
-    methods: {}
+    created () {
+      this.productId = this.$route.params.id
+    },
+    mounted () {
+      var list = Util.getLocal('LIKE_PRODUCT') || []
+      var index = list.indexOf(this.productId)
+      if (index >= 0) {
+        this.isLikeProduct = true
+      } else {
+        this.isLikeProduct = false
+      }
+    },
+    computed: {},
+    methods: {
+      onLike () {
+        // 这边本应该是操作数据库的，但我这里不做后台的服务，所以直接存入localstore
+        var list = Util.getLocal('LIKE_PRODUCT') || []
+        var index = list.indexOf(this.productId)
+        if (index >= 0) {
+          this.isLikeProduct = false
+          list.splice(index, 1)
+        } else {
+          this.isLikeProduct = true
+          list.push(this.productId)
+        }
+        Util.setLocal(list, 'LIKE_PRODUCT')
+      },
+      toService () {
+        alert('功能未开发')
+      },
+      toCartPage () {
+        this.$router.push({name: '购物车页'})
+      }
+    }
   }
 </script>
 
 <style lang="less" scoped>
-  .footer{
+  .footer {
     display: flex;
-    width:100%;
-    height:10vh;
+    width: 100%;
+    height: 10vh;
     position: fixed;
     bottom: 0;
     background-color: #fff;
     box-shadow: 0px -1px 10px #cccccc;
-    .icon{
+    .icon {
       flex: 1 1 auto;
       margin: auto;
       text-align: center;
       padding: 10px;
-      i{
-        font-size:5vw;
+      i {
+        font-size: 5vw;
+      }
+      .myLikeProduct {
+        color: #ff1908;
       }
       span.mint-badge.is-error.is-size-small {
         position: absolute;
       }
     }
-    .addCart{
-      height:100%;
-      width:40vw;
+    .addCart {
+      height: 100%;
+      width: 40vw;
       background-color: #f05423;
       display: flex;
-      p{
+      p {
         margin: auto;
         color: #fff;
-        font-weight:bold;
+        font-weight: bold;
       }
     }
   }
 
   @media screen and (min-width: 768px) {
-    .footer{
-      width:768px;
-      height:65px;
-      .icon{
-        i{
-          font-size:28px;
+    .footer {
+      width: 768px;
+      height: 65px;
+      .icon {
+        cursor: pointer;
+        i {
+          font-size: 28px;
         }
       }
-      .addCart{
-        height:100%;
-        width:268px;
+      .addCart {
+        cursor: pointer;
+        height: 100%;
+        width: 268px;
         background-color: #f05423;
         display: flex;
-        p{
+        p {
           margin: auto;
           color: #fff;
-          font-weight:bold;
+          font-weight: bold;
         }
       }
     }

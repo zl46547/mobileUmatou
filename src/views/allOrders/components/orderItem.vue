45549994d482bfcde99422de4d647bf7a8b2b109
xiaopai <xiaopai@suiyi.com.cn>
2019-09-04 18:10:28 +0800

    订单列表

--- src/views/allOrders/components/orderItem.vue
+++ src/views/allOrders/components/orderItem.vue
@@ -7,58 +7,73 @@
       </div>
       <p class="order-status-name">{{getOrderStatusName(orderItem.orderStatusCode)}}</p>
     </div>
-    <div class="content">
-      <div>
-        <div class="order-image">
-          <img v-for="item in orderItem.orderProductInfos"
-               :key="item._id"
-               :src="`http://picpro-sz.34580.com/sz/ImageUrl/${item.productInfo.pictureId}/120.jpeg`"
-               alt="订单图片"
-          />
-        </div>
-        <p class="order-count">共{{orderItem.orderProductInfos.length}}个</p>
+    <div class="content van-hairline--bottom">
+      <div class="order-image">
+        <img v-for="item in orderItem.orderProductInfos"
+             :key="item._id"
+             :src="`http://picpro-sz.34580.com/sz/ImageUrl/${item.productInfo.pictureId}/120.jpeg`"
+             alt="订单图片"
+        />
       </div>
-      <!--<i class="iconfont icon-arrow-right"></i>-->
+      <div class="order-count">
+        <span>共{{orderItem.orderProductInfos.length}}个</span>
+        <i class="iconfont icon-arrow-right"></i>
+      </div>
     </div>
-    <!--<div class="footer">-->
-    <!--<div class="price">-->
-    <!--<span>实付金额</span>-->
-    <!--<span>¥{{orderItem.finalPrice}}</span>-->
-    <!--</div>-->
-    <!--<div class="footer-btn">-->
-
-    <!--&lt;!&ndash; 支付订单按钮 &ndash;&gt;-->
-    <!--<div v-if=" orderItem.orderStatusName === '下单成功'" @click="goToPay(orderItem)">-->
-    <!--<OrderButton :button="buttonList[4]"></OrderButton>-->
-    <!--</div>-->
-    <!--&lt;!&ndash; 重新下单按钮 &ndash;&gt;-->
-    <!--<div v-if=" orderItem.orderStatusName === '订单过期'" @click="goToReOrder(orderItem)">-->
-    <!--<OrderButton :button="buttonList[2]"></OrderButton>-->
-    <!--</div>-->
-    <!--&lt;!&ndash; 确认订单按钮 &ndash;&gt;-->
-    <!--<div v-if="orderItem.orderStatus == 'PS'" @click="goToComfirm(orderItem)">-->
-    <!--<OrderButton :button="buttonList[1]"></OrderButton>-->
-    <!--</div>-->
-    <!--&lt;!&ndash; 评论按钮 &ndash;&gt;-->
-    <!--<div v-if="orderItem.orderStatus == 'FS'" @click="goToRate(orderItem)">-->
-    <!--<OrderButton :button="buttonList[0]"></OrderButton>-->
-    <!--</div>-->
-    <!--&lt;!&ndash; 退货按钮 &ndash;&gt;-->
-    <!--<div v-if="orderItem.orderStatus == 'FS'">-->
-    <!--<OrderButton :button="buttonList[5]"></OrderButton>-->
-    <!--</div>-->
-    <!--&lt;!&ndash; 删除订单按钮 &ndash;&gt;-->
-    <!--<div @click="delOrderAlert(orderItem)">-->
-    <!--<OrderButton :button="buttonList[3]"></OrderButton>-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--</div>-->
+    <div class="footer">
+      <div class="price">
+        <span>实付金额</span>
+        <span>¥{{orderItem.finalPrice}}</span>
+      </div>
+      <div class="footer-btn">
+        <Button v-show="orderItem.orderStatusCode==='OS'"
+                @click="goToPay(orderItem)"
+                color="#f04a18"
+                size="small"
+                plain round
+        >
+          立即支付
+        </Button>
+        <Button v-show="orderItem.orderStatusCode==='OS'"
+                @click="goToReOrder(orderItem)"
+                color="#909090"
+                size="small"
+                plain round
+        >
+          重新下单
+        </Button>
+        <!--&lt;!&ndash; 支付订单按钮 &ndash;&gt;-->
+        <!--<div v-if=" orderItem.orderStatusName === '下单成功'" @click="goToPay(orderItem)">-->
+        <!--<OrderButton :button="buttonList[4]"></OrderButton>-->
+        <!--</div>-->
+        <!--&lt;!&ndash; 重新下单按钮 &ndash;&gt;-->
+        <!--<div v-if=" orderItem.orderStatusName === '订单过期'" @click="goToReOrder(orderItem)">-->
+        <!--<OrderButton :button="buttonList[2]"></OrderButton>-->
+        <!--</div>-->
+        <!--&lt;!&ndash; 确认订单按钮 &ndash;&gt;-->
+        <!--<div v-if="orderItem.orderStatus == 'PS'" @click="goToComfirm(orderItem)">-->
+        <!--<OrderButton :button="buttonList[1]"></OrderButton>-->
+        <!--</div>-->
+        <!--&lt;!&ndash; 评论按钮 &ndash;&gt;-->
+        <!--<div v-if="orderItem.orderStatus == 'FS'" @click="goToRate(orderItem)">-->
+        <!--<OrderButton :button="buttonList[0]"></OrderButton>-->
+        <!--</div>-->
+        <!--&lt;!&ndash; 退货按钮 &ndash;&gt;-->
+        <!--<div v-if="orderItem.orderStatus == 'FS'">-->
+        <!--<OrderButton :button="buttonList[5]"></OrderButton>-->
+        <!--</div>-->
+        <!--&lt;!&ndash; 删除订单按钮 &ndash;&gt;-->
+        <!--<div @click="delOrderAlert(orderItem)">-->
+        <!--<OrderButton :button="buttonList[3]"></OrderButton>-->
+        <!--</div>-->
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import OrderButton from './orderButton.vue'
-  import {ORDER_STATUS_NAME} from '../../../util/enum'
+  import { ORDER_STATUS_NAME } from '../../../util/enum'
+  import { Button } from 'vant'
 
   export default {
     props: {
@@ -67,7 +82,7 @@
         type: Object
       }
     },
-    data() {
+    data () {
       return {
         selected: '',
         buttonList: [
@@ -105,24 +120,24 @@
       }
     },
     components: {
-      OrderButton
+      Button
     },
     methods: {
       /**
        * @param status 订单状态名称
        */
-      getOrderStatusName(status) {
+      getOrderStatusName (status) {
         return ORDER_STATUS_NAME[status]
       },
       /**
        * 删除订单提示框
        * @param val 删除的对象
        */
-      delOrderAlert(val) {
+      delOrderAlert (val) {
         let vm = this
         vm.$message({
           description: '确认删除该订单？',
-          onComfirm() {
+          onComfirm () {
             vm.delOrder(val)
           }
         })
@@ -131,7 +146,7 @@
        * 删除订单
        * @param val 删除的对象
        */
-      delOrder(val) {
+      delOrder (val) {
         var vm = this
         vm.allOrders.removeAll(function (t) {
           return t.orderNo === val.orderNo
@@ -147,7 +162,7 @@
        * 跳转到支付页面
        * @param val
        */
-      goToPay(val) {
+      goToPay (val) {
         var vm = this
         vm.$store.commit('SUBMIT_ORDER', val)
         vm.router.push({name: '支付订单'})
@@ -156,13 +171,13 @@
        * 重新下单
        * @param val
        */
-      goToReOrder(reOrders) {
+      goToReOrder (reOrders) {
         var vm = this
         // 将商品添加到购物车
         var carlist = vm.$store.state.car.carList
         vm.$message({
           description: `确认添加${reOrders.orderList.length}个商品到购物车？`,
-          onComfirm() {
+          onComfirm () {
             // 设置默认选中
             reOrders.orderList.forEach(e => {
               e.checked = true
@@ -179,11 +194,11 @@
        * 确认收货
        * @param val
        */
-      goToComfirm(val) {
+      goToComfirm (val) {
         var vm = this
         vm.$message({
           description: '是否确认收货？',
-          onComfirm() {
+          onComfirm () {
             vm.allOrders.forEach((t) => {
               if (t.orderNo === val.orderNo) {
                 t.orderStatus = 'FS'
@@ -199,7 +214,7 @@
        * 评价
        * @param val
        */
-      goToRate(val) {
+      goToRate (val) {
         var vm = this
         val.orderStatus = 'CLOSE'
         val.orderStatusName = '订单已关闭'
@@ -221,73 +236,53 @@
       justify-content: space-between;
 
       .order-info {
-        font-size: 26rem/@baseFontSize;
+        font-size: 28rem/@baseFontSize;
+        color: #333;
       }
 
       .order-status-name {
-        font-size: 28rem/@baseFontSize;
+        font-size: 30rem/@baseFontSize;
         color: @priceColor;
       }
     }
 
     .content {
-      background-color: #fff;
       padding: 20rem/@baseFontSize 25rem/@baseFontSize;
       display: flex;
       align-items: center;
       justify-content: space-between;
-
-      > div:nth-of-type(1) {
-        display: flex;
-        align-items: center;
-        /*.imgWidth {*/
-        /*width: calc(60vw + 18px);*/
-        /*display: flex;*/
-        /*overflow-x: auto;*/
-        /*overflow-y: hidden;*/
-        /*-webkit-overflow-scrolling: touch;*/
-        /*&::-webkit-scrollbar {*/
-        /*display: none*/
-        /*}*/
-        /*}*/
-
-        .order-image {
-          display: flex;
-          img {
-            display: block;
-            width: 120rem/@baseFontSize;
-            padding: 16rem/@baseFontSize;
-          }
-        }
-        .order-count {
-          margin-left: 20rem/@baseFontSize;
-          font-size: 30rem/@baseFontSize;
+      .order-image {
+        img {
+          width: 120rem/@baseFontSize;
+          padding: 16rem/@baseFontSize;
         }
       }
-
-      .icon-arrow-right {
-        font-size: 28rem/@baseFontSize;
+      .order-count {
+        font-size: 30rem/@baseFontSize;
+        display: flex;
+        align-items: center;
+        color: #333;
+        .icon-arrow-right {
+          font-size: 36rem/@baseFontSize;
+        }
       }
     }
 
     .footer {
-      padding: 10px 15px;
-      background-color: #fff;
+      padding: 20rem/@baseFontSize 25rem/@baseFontSize;
       display: flex;
       justify-content: space-between;
       align-items: center;
-      border-top: 1px solid #eee;
-
       .price {
         span:nth-of-type(1) {
           color: #9a9a9a;
-          font-size: 1.25rem;
+          font-size: 28rem/@baseFontSize;
           margin-right: 5px;
         }
 
         span:nth-of-type(2) {
           color: #ff156a;
-          font-size: 1.4rem;
+          font-size: 28rem/@baseFontSize;
         }
       }
 
